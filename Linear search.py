aade3a77fbee926c7c80e209a1f03c3fece9e8eb
Soumi2808 <144762865+Soumi2808@users.noreply.github.com>
2023-09-22 20:27:05 +0530

    Add files via upload

---
+++ Linear search.py
@@ -0,0 +1,11 @@
+print("****LINEAR_SEARCH_PRODUCT****")
+def linear_search_product(product_list, target_product):
+    indices = []
+    for index, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(index)
+    return indices
+products = ["grapes", "banana", "apple", "grapes", "apple"]
+target = "grapes"
+result = linear_search_product(products, target)
+print(result)  
